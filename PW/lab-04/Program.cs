5743e13f12f3c8b2c1ca8c5e93d4300d5d373e72
Jakub Błażejowski <56088851+Jorewin@users.noreply.github.com>
2022-11-06 21:27:25 +0100

    Add PW/lab-04

---
+++ PW/lab-04/Program.cs
@@ -0,0 +1,27 @@
+﻿TestDelegate testDelegate = new TestDelegate() {_s="new"};
+TestDelegate.PrintIt printIt = new TestDelegate.PrintIt(TestDelegate.StaticPrintIt);
+Console.WriteLine(printIt("Hello"));
+
+TestDelegate.PrintIt printIt2 = new TestDelegate.PrintIt(testDelegate.MethodPrintIt);
+Console.WriteLine(printIt2("Hi"));
+
+public class TestDelegate
+{
+    public string _s = "";
+
+    public static string StaticPrintIt(string s)
+    {
+        Console.WriteLine($"StaticPrintIt prints {s}");
+        return "StaticPrintIt return value";
+    }
+
+    public delegate string PrintIt(string s);
+
+    public string MethodPrintIt(string s)
+    {
+        Console.WriteLine($"MethodPrintIt prints {s} {this._s}");
+        return "MethodPrintIt return value";
+    }
+
+    public PrintIt AnonymousPrintIt = new TestDelegate.PrintIt { return ""; };
+}
