f9e14c5843dcb3ee193ebcfa5d982813083aac5c
Jakub Błażejowski <56088851+Jorewin@users.noreply.github.com>
2022-11-17 11:58:25 +0100

    Update PW/lab-04

--- PW/lab-04/Program.cs
+++ PW/lab-04/Program.cs
@@ -1,21 +1,40 @@
-﻿TestDelegate testDelegate = new TestDelegate() {_s="new"};
-TestDelegate.PrintIt printIt = new TestDelegate.PrintIt(TestDelegate.StaticPrintIt);
-Console.WriteLine(printIt("Hello"));
+﻿TestDelegate testDelegate = new TestDelegate() {_s="new", printIt = TestDelegate.StaticPrintIt};
+Console.WriteLine(testDelegate.printIt("Hello"));
 
-TestDelegate.PrintIt printIt2 = new TestDelegate.PrintIt(testDelegate.MethodPrintIt);
-Console.WriteLine(printIt2("Hi"));
+TestDelegate testDelegate2 = new TestDelegate() {_s="new2", printIt = testDelegate.MethodPrintIt};
+Console.WriteLine(testDelegate2.printIt("Hi"));
+
+TestDelegate testDelegate3 = new TestDelegate() {_s="new3", printIt = TestDelegate.StaticPrintIt};
+testDelegate3.printIt += testDelegate.MethodPrintIt;
+Console.WriteLine(testDelegate3.printIt("Bonjour"));
+
+TestDelegate testDelegate4 = new TestDelegate() {_s="new4"};
+Console.WriteLine(testDelegate4.printIt("Hola"));
+
+public delegate string PrintIt(string s);
+public delegate string PrintItAnonymous(string s);
 
 public class TestDelegate
 {
-    public string _s = "";
+    public TestDelegate()
+    {
+        this.printIt = delegate(string s)
+        {
+            Console.WriteLine($"AnonymousPrintIt prints {s} {this._s} {this._t}");
+            return "AnonymousPrintIt return value";
+        };
+    }
 
+    public string _s = "";
+    private string _t = "secret";
+
+    public PrintIt printIt;
     public static string StaticPrintIt(string s)
     {
         Console.WriteLine($"StaticPrintIt prints {s}");
         return "StaticPrintIt return value";
     }
 
-    public delegate string PrintIt(string s);
 
     public string MethodPrintIt(string s)
     {
@@ -23,5 +42,5 @@ public class TestDelegate
         return "MethodPrintIt return value";
     }
 
-    public PrintIt AnonymousPrintIt = new TestDelegate.PrintIt { return ""; };
+//    public PrintIt AnonymousPrintIt = new TestDelegate.PrintIt { return ""; };
 }
